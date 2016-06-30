15a60633b410a3e3d21b55e35b18572d596c04b7
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-06-30 08:41:28 -0700

    Update home test.

--- test/components/Home.test.js
+++ test/components/Home.test.js
@@ -2,10 +2,16 @@ import Home from "components/Home";
 import { shallow } from "enzyme";
 
 describe("components/Home", () => {
+  const testCheeses = {cheese1 : {
+    name : 'Cheese 1',
+    image : 'image.jpg',
+    desc : 'Description 1',
+    rating : 10
+  }};
+
   it("renders without an issue", () => {
-    const subject = <Home />;
+    const subject = <Home cheeses={testCheeses} />;
     const wrapper = shallow(subject);
     expect(wrapper).to.exist;
-  });
+  });;
 });
-
