2e96ad5c74c2e77f682c2636496e4ed7cd1f47b7
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-07-04 22:25:11 -0700

    Filter cheeses by rating in Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,23 @@ import React, { Component } from "react";
 import CheeseList from './CheeseList';
 
 export default class Home extends Component {
-  render () {
+  topCheeseFilter(numberToReturn) {
+    var cheeses = this.props.cheeses;
+    return Object.keys(cheeses).sort(function(a, b){
+      a = cheeses[a].rating;
+      b = cheeses[b].rating;
+      return ((a > b) ? -1 : ((a < b) ? 1 : 0));
+    }).slice(0, numberToReturn).reduce(function(result, key){
+      result[key] = cheeses[key];
+      return result;
+    }, {});
+  }
+
+  render() {
     return (
       <div>
-        <h2>Top 10 Cheeses</h2>
-        <CheeseList cheeses={this.props.cheeses} />
+        <h2>Top 4 Cheeses</h2>
+        <CheeseList cheeses={this.topCheeseFilter(4)} />
       </div>
     );
   }
